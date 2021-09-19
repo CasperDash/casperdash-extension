35959c915656cc9ff89237812e62a923a557c80e
kiennguyen <kiennguyen@niometrics.com>
2021-09-19 15:56:39 +0700

    fix const

--- client/src/constants/key.js
+++ client/src/constants/key.js
@@ -1,11 +1,12 @@
-const config = () => {
-	try {
-		return JSON.parse(localStorage.getItem('configuration'));
-	} catch {
-		return {};
-	}
-};
+// const config = () => {
+// 	try {
+// 		return JSON.parse(localStorage.getItem('configuration'));
+// 	} catch {
+// 		return {};
+// 	}
+// };
 
-export const { MOTE_RATE, TESTNET_RPC_URL } = config();
+export const MOTE_RATE = 1000000000;
+export const TESTNET_RPC_URL = 'http://16.162.124.124:7777/rpc';
 export const KEY_PREFIX = ['account-hash-', 'uref-'];
 export const MIN_TRANSFER = 2.5;
