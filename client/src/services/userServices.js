9c989b8da1a62286be042ee57817f5fa677cd3b1
kiennguyen <kiennguyen@niometrics.com>
2021-09-22 14:42:05 +0700

    default transaction id

--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -4,12 +4,14 @@ import { getTransferDeploy } from './casperServices';
 
 export const getSignedTransferDeploy = async (transactionDetail = {}) => {
 	try {
-		const { fromAddress, toAddress, amount, transactionId } = transactionDetail;
+		const { fromAddress, toAddress, amount, transactionId = 0 } = transactionDetail;
 		const fromPbKey = CLPublicKey.fromHex(fromAddress);
 		const toPbKey = CLPublicKey.fromHex(toAddress);
 		const transferDeploy = getTransferDeploy(fromPbKey, toPbKey, amount * MOTE_RATE, transactionId);
 		const deployObj = DeployUtil.deployToJson(transferDeploy);
+		console.log(deployObj);
 		const signedDeploy = await Signer.sign(deployObj, fromAddress, toAddress);
+
 		return signedDeploy;
 	} catch (error) {
 		console.log(error);
