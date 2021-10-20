eb63318ee85019f84fb6c030a48fb4013bbfdadd
kiennguyen <kiennguyen@niometrics.com>
2021-10-20 15:59:19 +0700

    Validate password

--- client/src/components/Accounts/CreateWallet.jsx
+++ client/src/components/Accounts/CreateWallet.jsx
@@ -8,6 +8,9 @@ const STEPS = [CreatePassword, MnemonicForm];
 export const CreateWallet = () => {
 	const [step, setStep] = useState(0);
 	const CurrentStepComp = STEPS[step] || STEPS[0];
+	const [hasError, setHasError] = useState(false);
+	const [finalPassword, setFinalPassword] = useState('');
+	const disabled = hasError || !finalPassword;
 	return (
 		<section className="cd_create_wallet_section">
 			<div className="cd_create_wallet_content container">
@@ -15,16 +18,24 @@ export const CreateWallet = () => {
 					<h3 className="cd_create_wallet_heading">Create New Wallet</h3>
 				</div>
 
-				<CurrentStepComp />
+				<CurrentStepComp
+					finalPassword={finalPassword}
+					setFinalPassword={setFinalPassword}
+					setHasError={setHasError}
+				/>
 
-				<div className="cd_securebackup_btn">
+				<div className="cd_create_wallet_btn">
 					{step > 0 && (
 						<Link to={'#'} className="mx-auto" onClick={() => setStep(step - 1)}>
 							Back
 						</Link>
 					)}
 					{step < STEPS.length - 1 && (
-						<Link to={'#'} className="mx-auto" onClick={() => setStep(step + 1)}>
+						<Link
+							to={'#'}
+							className={`mx-auto ${disabled ? 'disabled' : ''}`}
+							onClick={() => !disabled && setStep(step + 1)}
+						>
 							Next
 						</Link>
 					)}
