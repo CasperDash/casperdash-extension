a81af4e768a67a4334868473b7791bf8d84cdccd
kiennguyen <kiennguyen@niometrics.com>
2021-10-23 10:21:44 +0700

    Login

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { checkExistWallet } from '../../services/userServices';
+import { Login } from './Login';
 
 const Home = () => {
+	const hasWallet = checkExistWallet();
 	return (
 		<section className="cd_welcome_slide_section">
 			<div className="cd_welcome_slide_content container">
-				<h2 className="cd_welcome_slide_heading">Welcome to CasperDash</h2>
-				<p className="cd_welcome_slide_peregraph">A Casper Wallet.</p>
-				<Link to={'/newwallet'} className="cd_welcome_slide_step_btns">
-					Create Wallet
-				</Link>
-				<Link to={'/newwallet'} className="cd_welcome_slide_already_wallet">
-					I already have wallet
-				</Link>
+				<div>
+					<h2 className="cd_welcome_slide_heading">Welcome to CasperDash</h2>
+					<p className="cd_welcome_slide_peregraph">A Casper Wallet.</p>
+				</div>
+				{hasWallet ? (
+					<Login />
+				) : (
+					<>
+						<Link to={'/newwallet'} className="cd_welcome_slide_step_btns">
+							Create Wallet
+						</Link>
+						<Link to={'/newwallet'} className="cd_welcome_slide_already_wallet">
+							I already have wallet
+						</Link>
+					</>
+				)}
 			</div>
 		</section>
 	);
