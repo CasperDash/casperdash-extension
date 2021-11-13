b7ce2c903b2802610a28c032609d3abfd906570c
Thinh Nguyen <thinhnp@ymese.com>
2021-11-13 16:18:49 +0700

    init form comp

---
+++ client/src/components/Common/Layout/Stake/Form.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Formik, Field } from 'formik';
+import { Container, Row, Button, Form, FormControl } from 'react-bootstrap';
+import Select from 'react-select';
+
+/**
+ * Wrap releact-select to work with Formik.
+ *
+ * @param {Object}
+ * @returns
+ */
+const SelectField = ({ options, field, form }) => (
+	<Select
+		options={options}
+		name={field.name}
+		value={options ? options.find((option) => option.value === field.value) : ''}
+		onChange={(option) => form.setFieldValue(field.name, option.value)}
+		onBlur={field.onBlur}
+	/>
+);
+
+const StakingForm = () => {
+	const options = [
+		{ value: 'chocolate', label: 'Chocolate' },
+		{ value: 'strawberry', label: 'Strawberry' },
+		{ value: 'vanilla', label: 'Vanilla' },
+	];
+
+	const handleSubmit = (values) => {
+		console.log('Submit', values);
+	};
+
+	return (
+		<div>
+			<Formik initialValues={{ amount: 1 }} onSubmit={handleSubmit}>
+				{({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+					<Form noValidate onSubmit={handleSubmit}>
+						<Form.Group className="mb-3" controlId="cd-staking-amount">
+							<FormControl name="amount" type="number" placeholder="Amount" required />
+						</Form.Group>
+						<Form.Group className="mb-3" controlId="cd-staking-validator">
+							<Field name={'example'} component={SelectField} options={options} />
+						</Form.Group>
+						<Button variant="secondary" type="submit">
+							Stake
+						</Button>
+					</Form>
+				)}
+			</Formik>
+		</div>
+	);
+};
+
+export default StakingForm;
