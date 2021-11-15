13b290b02894b470b65e7519a5c9fb471afd5755
Thinh Nguyen <thinhnp@ymese.com>
2021-11-15 22:40:56 +0700

    group stake by validator

--- client/src/selectors/stake.js
+++ client/src/selectors/stake.js
@@ -1,5 +1,21 @@
-export const getStakeDeploys =
+export const getStakeDeploysGroupByValidator =
 	() =>
 	({ stakes = {} }) => {
+		if (stakes.stakes) {
+			let groupByValidators = [];
+			stakes.stakes.forEach((stake) => {
+				const { validator, amount } = stake;
+				const foundValidator = groupByValidators.findIndex((item) => validator === item.validator);
+				if (foundValidator < 0) {
+					groupByValidators.push({
+						validator,
+						amount,
+					});
+				} else {
+					groupByValidators[foundValidator].amount += amount;
+				}
+			});
+			return groupByValidators;
+		}
 		return stakes;
 	};
