7bb9eb884377b368aaea3551aeb08e439a061620
kiennguyen <kiennguyen@niometrics.com>
2021-11-21 23:42:48 +0700

    Prettier

--- client/src/components/Tokens/index.test.jsx
+++ client/src/components/Tokens/index.test.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-multi-comp */
 import React from 'react';
 import * as redux from 'react-redux';
-import { render, fireEvent, cleanup,act } from '@testing-library/react';
+import { render, fireEvent, cleanup, act } from '@testing-library/react';
 import Token from './index';
 
 //Set up
@@ -12,7 +12,7 @@ jest.mock('../../actions/tokensActions', () => {
 		addCustomTokenAddressToLocalStorage: () => {},
 		getTokenAddressFromLocalStorage: () => {},
 		fetchTokensInfoWithBalance: () => {},
-		getTokenInfo:()=>({error:{message:'error message'}})
+		getTokenInfo: () => ({ error: { message: 'error message' } }),
 	};
 });
 
@@ -58,19 +58,22 @@ test('Show no token message', () => {
 });
 
 test('Have Tokens', () => {
-	spyOnUseSelector.mockReturnValue([
-		{ symbol: 'CDAS', address: 'cdashaddress',balance:{displayValue:1000} },
-		{ symbol: 'BTC', address: 'btcaddress' },
-	]).mockReturnValueOnce([
-		{ symbol: 'CDAS', address: 'cdashaddress',balance:{displayValue:1000} },
-		{ symbol: 'BTC', address: 'btcaddress' },
-	]).mockReturnValueOnce([]).mockReturnValueOnce({balance:{displayBalance:2000}});
+	spyOnUseSelector
+		.mockReturnValue([
+			{ symbol: 'CDAS', address: 'cdashaddress', balance: { displayValue: 1000 } },
+			{ symbol: 'BTC', address: 'btcaddress' },
+		])
+		.mockReturnValueOnce([
+			{ symbol: 'CDAS', address: 'cdashaddress', balance: { displayValue: 1000 } },
+			{ symbol: 'BTC', address: 'btcaddress' },
+		])
+		.mockReturnValueOnce([])
+		.mockReturnValueOnce({ balance: { displayBalance: 2000 } });
 
-	const { getByText,queryAllByText } = render(<Token />);
+	const { getByText } = render(<Token />);
 	expect(getByText(/cdashaddress/i).textContent).toBe('cdashaddress');
 	expect(getByText(/BTC/i).textContent).toBe('BTC');
 	expect(getByText(/1,000/i).textContent).toBe('1,000');
-	
 });
 
 test('Add new token, add token modal should be shown', () => {
@@ -84,48 +87,48 @@ test('Add new token, add token modal should be shown', () => {
 	expect(container.querySelector('.cd_add_token_modal_content')).toBe(null);
 });
 
-test('Show error if can not add token',async ()=>{
+test('Show error if can not add token', async () => {
 	spyOnUseSelector.mockReturnValue([]);
-	spyOnUseDispatch.mockReturnValue(()=>({error:'error message'}));
+	spyOnUseDispatch.mockReturnValue(() => ({ error: 'error message' }));
 	const { getByText } = render(<Token />);
 	fireEvent.click(getByText('+ Add Token'));
 	await act(async () => {
 		/* fire events that update state */
 		fireEvent.click(getByText('Add'));
-	  });
+	});
 	expect(getByText(/error message/i).textContent).toBe('error message');
-})
+});
 
-test('Close add token modal if successfully add token',async ()=>{
+test('Close add token modal if successfully add token', async () => {
 	spyOnUseSelector.mockReturnValue([]);
-	spyOnUseDispatch.mockReturnValue(()=>({data:{name:'Casper Dash'}}));
-	const { getByText,queryByText } = render(<Token />);
+	spyOnUseDispatch.mockReturnValue(() => ({ data: { name: 'Casper Dash' } }));
+	const { getByText, queryByText } = render(<Token />);
 	fireEvent.click(getByText('+ Add Token'));
 	await act(async () => {
 		/* fire events that update state */
 		fireEvent.click(getByText('Add'));
-	  });
+	});
 	expect(queryByText('Add')).toBe(null);
-})
+});
 
-test('Set selected token when click on token area',async ()=>{
+test('Set selected token when click on token area', async () => {
 	spyOnUseSelector.mockReturnValue([
-		{ symbol: 'CDAS', address: 'cdashaddress',name:'Casper Dash' },
-		{ symbol: 'BTC', address: 'btcaddress',name:'Bitcoin' },
+		{ symbol: 'CDAS', address: 'cdashaddress', name: 'Casper Dash' },
+		{ symbol: 'BTC', address: 'btcaddress', name: 'Bitcoin' },
 	]);
-	const { getByText,queryByText } = render(<Token />);
+	const { getByText, queryByText } = render(<Token />);
 	fireEvent.click(getByText('BTC'));
 	expect(queryByText('btcaddress').textContent).toBe('btcaddress');
-})
+});
 
-test('Show error if click on add token without public key',async ()=>{
+test('Show error if click on add token without public key', async () => {
 	spyOnUseSelector.mockReturnValue('');
-	const { getByText,queryByText,queryAllByText } = render(<Token />);
+	const { getByText, queryByText, queryAllByText } = render(<Token />);
 	fireEvent.click(getByText('+ Add Token'));
 	expect(queryByText('Unlock your Signer!').textContent).toBe('Unlock your Signer!');
 	await act(async () => {
 		/* fire events that update state */
 		fireEvent.click(queryAllByText('Close')[0]);
-	  });
-	  expect(queryByText('Unlock your Signer!')).toBe(null);
-})
+	});
+	expect(queryByText('Unlock your Signer!')).toBe(null);
+});
