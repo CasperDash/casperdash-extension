0f2bfaf8bc3030b76ecdad40a6f2208ed22bbe6f
Thinh Nguyen <thinhnp@ymese.com>
2021-11-22 14:31:04 +0700

    Define STAKE action type

--- client/src/actions/stakeActions.js
+++ client/src/actions/stakeActions.js
@@ -1,15 +1,15 @@
-import { DEPLOY, VALIDATORS } from '../store/actionTypes';
+import { STAKE, VALIDATORS } from '../store/actionTypes';
 import { setLocalStorageValue } from '../services/localStorage';
 
 const LOCAL_STORAGE_STAKE_PATH = 'deploys.stakes';
 
 export const pushStakeToLocalStorage = (publicKey, value) => (dispatch) => {
 	setLocalStorageValue(publicKey, LOCAL_STORAGE_STAKE_PATH, value, 'push');
-	dispatch({ type: DEPLOY.PUSH_STAKE_TO_LOCAL_STORAGE, payload: value });
+	dispatch({ type: STAKE.PUSH_STAKE_TO_LOCAL_STORAGE, payload: value });
 };
 
 export const getStakeFromLocalStorage = (publicKey) => ({
-	type: DEPLOY.GET_STAKES_FROM_LOCAL_STORAGE,
+	type: STAKE.GET_STAKES_FROM_LOCAL_STORAGE,
 	payload: publicKey,
 });
 
--- client/src/store/reducers/stakes.js
+++ client/src/store/reducers/stakes.js
@@ -1,12 +1,12 @@
 import { getLocalStorageValue } from '../../services/localStorage';
-import { DEPLOY } from '../actionTypes';
+import { STAKE } from '../actionTypes';
 
 export default function userReducer(state = { delegations: [] }, action) {
 	switch (action.type) {
-		case DEPLOY.PUSH_STAKE_TO_LOCAL_STORAGE:
+		case STAKE.PUSH_STAKE_TO_LOCAL_STORAGE:
 			const delegations = state.delegations || [];
 			return { ...state, delegations: [...delegations, action.payload] };
-		case DEPLOY.GET_STAKES_FROM_LOCAL_STORAGE:
+		case STAKE.GET_STAKES_FROM_LOCAL_STORAGE:
 			const items = getLocalStorageValue(action.payload, 'deploys.stakes');
 			return { ...state, delegations: items };
 		default:
