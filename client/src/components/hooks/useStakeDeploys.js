1d40b387073c7f047be9437b00710eac2eef132c
Thinh Nguyen <thinhnp@ymese.com>
2021-11-24 16:39:37 +0700

    Add pending amount for existed validator

--- client/src/components/hooks/useStakeDeploys.js
+++ client/src/components/hooks/useStakeDeploys.js
@@ -1,21 +1,50 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { moteToCspr } from '../../helpers/balance';
+import { getPendingStakes } from '../../selectors/stake';
 import { getValidators } from '../../selectors/validator';
+import { useAutoRefreshEffect } from './useAutoRefreshEffect';
+import { getTransferDeploysStatus, updateTransferDeployStatus } from '../../actions/deployActions';
 
 export const useStakeFromValidators = (publicKey) => {
+	const dispatch = useDispatch();
+
 	const validators = useSelector(getValidators);
-	let result = [];
+	const pendingStakes = useSelector(getPendingStakes());
+
+	useAutoRefreshEffect(() => {
+		if (!pendingStakes.length) {
+			return;
+		}
+		(async () => {
+			const { data } = await dispatch(getTransferDeploysStatus(pendingStakes.map((stake) => stake.deployHash)));
+			dispatch(updateTransferDeployStatus(publicKey, 'deploys.stakes', data));
+		})();
+	}, [JSON.stringify(pendingStakes), dispatch]);
+
+	let stakedValidators = [];
 	validators.forEach((validator) => {
 		if (!validator.bidInfo) {
 			return;
 		}
-		const foundDelegator = validator.bidInfo.bid.delegators.find((delegator) => publicKey == delegator.public_key);
-		if (foundDelegator) {
-			result.push({
-				validator: validator.public_key,
-				stakedAmount: moteToCspr(foundDelegator.staked_amount),
-			});
+		const foundDelegator = validator.bidInfo.bid.delegators.find(
+			(delegator) => publicKey.toLowerCase() == delegator.public_key.toLowerCase(),
+		);
+
+		if (!foundDelegator) {
+			return;
 		}
+
+		const { public_key: validatorPublicKey } = validator;
+		const pendingStake = pendingStakes.find((pendingStake) => pendingStake.validator === validatorPublicKey);
+		let stakedValidator = {
+			validator: validatorPublicKey,
+			stakedAmount: moteToCspr(foundDelegator.staked_amount),
+		};
+		if (pendingStake) {
+			stakedValidator.pendingAmount = pendingStake.amount;
+		}
+
+		stakedValidators.push(stakedValidator);
 	});
-	return result;
+	return stakedValidators;
 };
