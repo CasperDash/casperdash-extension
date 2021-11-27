eacdf10487feb5c59863eb7f45f9a3224627f99e
Thinh Nguyen <thinhnp@ymese.com>
2021-11-27 11:57:23 +0700

    Add code comment

--- client/src/helpers/currency.js
+++ client/src/helpers/currency.js
@@ -2,7 +2,22 @@ import { BigNumber } from '@ethersproject/bignumber';
 import { MOTE_RATE } from '../constants/key';
 import Big from 'big.js';
 
+/**
+ * /**
+ * Conver CSPR to Motes.
+ *
+ * Inpsired from toWie implementation (https://github.com/ethjs/ethjs-unit/blob/master/src/index.js#L119)
+ * It will convert to String number | number to Big Number (We use big.js to cover the float numbers).
+ * After that multiple with mote rate 10⁸
+ *
+ * @param {Number|String} amount
+ * @returns {BigNumberis|String} Return "-" if it's the invalid big number input.
+ */
 export const toMotes = (amount) => {
-	const bigAmount = Big(amount).times(MOTE_RATE).round(0, Big.roundDown).toString();
-	return BigNumber.from(bigAmount);
+	try {
+		const bigAmount = Big(amount).times(MOTE_RATE).round(0, Big.roundDown).toString();
+		return BigNumber.from(bigAmount);
+	} catch (error) {
+		return '-';
+	}
 };
