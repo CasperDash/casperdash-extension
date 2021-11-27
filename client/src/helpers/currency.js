3e7e11fbb55bdbd45ee68cde885bd935953cd43c
Thinh Nguyen <thinhnp@ymese.com>
2021-11-27 11:28:56 +0700

    Remove unused

--- client/src/helpers/currency.js
+++ client/src/helpers/currency.js
@@ -6,15 +6,3 @@ export const toMotes = (amount) => {
 	const bigAmount = Big(amount).times(MOTE_RATE).round(0, Big.roundDown).toString();
 	return BigNumber.from(bigAmount);
 };
-
-export const fromMotes = (amount) => {
-	return Big(amount).div(MOTE_RATE).toString();
-};
-
-export const csprToString = (amount) => {
-	try {
-		return Big(amount).round(9, Big.roundDown).toString();
-	} catch (error) {
-		return '-';
-	}
-};
