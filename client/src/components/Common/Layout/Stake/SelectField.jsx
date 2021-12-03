ca7da63f603f86320038932d24537ce125804352
Thinh Nguyen <thinhnp@ymese.com>
2021-12-03 15:56:49 +0700

    UT for select field

--- client/src/components/Common/Layout/Stake/SelectField.jsx
+++ client/src/components/Common/Layout/Stake/SelectField.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import Select from 'react-select';
+import selectEvent from 'react-select-event';
+
+const OptLabel = (e) => (
+	<div>
+		<div>
+			{e.icon} {e.label}
+		</div>
+		<div>
+			<small>Rate: {e.rate}%</small>
+		</div>
+	</div>
+);
 
 /**
  * Wrap releact-select to work with Formik.
@@ -7,24 +19,16 @@ import Select from 'react-select';
  * @param {Object}
  * @returns
  */
-const SelectField = ({ options, field, form }) => (
+const SelectField = ({ options, field, form, customOptLabel = OptLabel }) => (
 	<Select
 		options={options}
+		inputId={field.name}
 		name={field.name}
 		value={options ? options.find((option) => option.value === field.value) : null}
 		onChange={(option) => form.setFieldValue(field.name, option.value)}
 		onBlur={field.onBlur}
 		placeholder="Validator"
-		getOptionLabel={(e) => (
-			<div>
-				<div>
-					{e.icon} {e.label}
-				</div>
-				<div>
-					<small>Rate: {e.rate}%</small>
-				</div>
-			</div>
-		)}
+		getOptionLabel={customOptLabel}
 	/>
 );
 
---
+++ client/src/components/Common/Layout/Stake/SelectField.test.jsx
@@ -0,0 +1,59 @@
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import SelectField from './SelectField';
+import selectEvent from 'react-select-event';
+
+afterEach(cleanup);
+
+describe('SelectField', () => {
+	it('Can generate options', () => {
+		const options = [
+			{
+				icon: 'icon',
+				label: '0x123',
+			},
+		];
+
+		const { queryAllByText } = render(<SelectField options={options} field={{ name: 'validator' }} />);
+		// debug();
+		expect(queryAllByText('icon 0x123')[0].textContent).toBe('icon 0x123');
+	});
+
+	it('Can handle null value', () => {
+		const { queryAllByText, container } = render(<SelectField field={{ name: 'validator' }} />);
+		expect(queryAllByText('Validator')[0].textContent).toBe('Validator');
+	});
+
+	it('Can trigger on change event', async () => {
+		const form = {};
+		const options = [
+			{
+				icon: 'icon',
+				label: '0x123',
+				value: '0x123',
+			},
+			{
+				icon: 'icon 2',
+				label: '0x124',
+				value: '0x124',
+			},
+		];
+		form.setFieldValue = () => {};
+
+		const spy = jest.spyOn(form, 'setFieldValue');
+
+		const { container, getByLabelText, getByRole } = render(
+			<form role="form">
+				<label htmlFor="validator">ValidatorVal</label>
+				<SelectField
+					field={{ name: 'validator' }}
+					form={form}
+					options={options}
+					customOptLabel={(e) => e.label}
+				/>
+			</form>,
+		);
+		await selectEvent.select(getByLabelText('ValidatorVal'), '0x123');
+		expect(spy).toHaveBeenCalled();
+		spy.mockRestore();
+	});
+});
