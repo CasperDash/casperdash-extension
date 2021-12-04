30a1bb4dca156249a86f78e4ce135e20d7aac1aa
Thinh Nguyen <thinhnp@ymese.com>
2021-12-04 18:45:09 +0700

    UT for signer warning

---
+++ client/src/components/Common/Layout/Stake/UnlockSingerWarning.test.jsx
@@ -0,0 +1,21 @@
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import * as redux from 'react-redux';
+import UnlockSingerWarning from './UnlockSingerWarning';
+
+afterEach(cleanup);
+
+describe('UnlockSingerWarning', () => {
+	test('Should display warning message', async () => {
+		// Mock useSelector hook
+		const spyOnUseSelector = jest.spyOn(redux, 'useSelector');
+		const spyOnUseDispatch = jest.spyOn(redux, 'useDispatch');
+		const mockDispatch = jest.fn();
+		spyOnUseDispatch.mockReturnValue(mockDispatch);
+
+		spyOnUseSelector.mockReturnValue([]);
+		const { queryAllByText } = render(<UnlockSingerWarning title="Title" message={'Warning message'} />);
+
+		expect(queryAllByText('Title')[0].textContent).toBe('Title');
+		expect(queryAllByText('Warning message')[0].textContent).toBe('Warning message');
+	});
+});
