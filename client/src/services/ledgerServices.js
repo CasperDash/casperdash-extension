c705bd5bb19b96b0a1341f933efb9157b2c88615
Thinh Nguyen <thinhnp@ymese.com>
2021-12-23 15:09:48 +0700

    Use casper app from option

--- client/src/services/ledgerServices.js
+++ client/src/services/ledgerServices.js
@@ -1,11 +1,10 @@
 import { DeployUtil, CLPublicKey } from 'casper-js-sdk';
-import TransportWebUSB from '@ledgerhq/hw-transport-webusb';
-import CasperApp from '@zondax/ledger-casper';
 
 export const signByLedger = async (deployObj, options = {}) => {
-	const transport = await TransportWebUSB.create();
-	const app = new CasperApp(transport);
-	const responseDeploy = await app.sign(`m/44'/506'/0'/0/${options.keyPath}`, DeployUtil.deployToBytes(deployObj));
+	const responseDeploy = await options.app.sign(
+		`m/44'/506'/0'/0/${options.keyPath}`,
+		DeployUtil.deployToBytes(deployObj),
+	);
 	let signedDeploy = DeployUtil.setSignature(
 		deployObj,
 		responseDeploy.signatureRS,
