64135ca4492eba5202ae022654020bc858c5d4bf
Thinh Nguyen <thinhnp@ymese.com>
2021-12-23 15:09:36 +0700

    Pass casper app

--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -2,13 +2,13 @@ import { CLPublicKey } from 'casper-js-sdk';
 import { toMotes } from '../helpers/currency';
 import { getTransferDeploy, signDeploy } from './casperServices';
 
-export const getSignedTransferDeploy = async (transactionDetail = {}) => {
+export const getSignedTransferDeploy = async (transactionDetail = {}, casperApp) => {
 	try {
 		const { fromAddress, toAddress, amount, transferId = 0, fee } = transactionDetail;
 		const fromPbKey = CLPublicKey.fromHex(fromAddress);
 		const toPbKey = CLPublicKey.fromHex(toAddress);
 		const transferDeploy = getTransferDeploy(fromPbKey, toPbKey, toMotes(amount), transferId, fee);
-		const signedDeploy = await signDeploy(transferDeploy, fromAddress, toAddress);
+		const signedDeploy = await signDeploy(transferDeploy, fromAddress, toAddress, casperApp);
 
 		return signedDeploy;
 	} catch (error) {
