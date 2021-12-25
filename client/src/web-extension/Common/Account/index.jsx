7caa67719a7156609c6e6841af1ea198a90b86cf
kiennguyen <kiennguyen@niometrics.com>
2021-12-25 14:01:26 +0700

    Update dashboard layout

---
+++ client/src/web-extension/Common/Account/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+export const AccountInfo = () => {
+	return (
+		<div className="cd_we_account">
+			<div className="cd_we_account_name">Account 1</div>
+			<div className="cd_we_account_address">01123...123123</div>
+			<div className="cd_we_account_balance">$100</div>
+		</div>
+	);
+};
---
+++ client/src/web-extension/Dashboard/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Header } from '../Common/Header';
+import { SendReceive } from '../Common/SendReceive';
+import { Grid } from '../Common/Grid';
+import { AccountInfo } from '../Common/Account';
+import './index.scss';
+
+const WalletDetails = () => {
+	// Selector
+
+	return (
+		<section className="cd_we_dashboard_page">
+			<Header />
+			<div className="cd_we_content">
+				<div className="cd_we_main_content">
+					<AccountInfo />
+					<SendReceive />
+				</div>
+				<Grid data={new Array(10).fill()} />
+				<div className="cd_we_action"> + Add Custom Token</div>
+			</div>
+		</section>
+	);
+};
+
+export default WalletDetails;
