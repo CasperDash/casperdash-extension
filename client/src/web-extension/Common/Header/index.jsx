d577b0980cc5ac9a8a644485928a4f112ebfad11
kiennguyen <kiennguyen@niometrics.com>
2021-12-25 17:01:58 +0700

    update header image

--- client/src/web-extension/Common/Header/index.jsx
+++ client/src/web-extension/Common/Header/index.jsx
@@ -18,7 +18,7 @@ export const Header = () => {
 	return (
 		<div className="cd_we_header">
 			<div className="cd_we_logo">
-				<image src={casperDashLogo} alt="casperdash-logo" />
+				<img src={casperDashLogo} alt="casperdash-logo" />
 			</div>
 			<div className="cd_we_settings">
 				<i className="bi bi-gear" />
