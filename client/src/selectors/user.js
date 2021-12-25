2e40f9fbeaa9ceb34340746d495a49c39dfe6309
kiennguyen <kiennguyen@niometrics.com>
2021-12-25 14:57:13 +0700

    get total account balance

--- client/src/selectors/user.js
+++ client/src/selectors/user.js
@@ -2,9 +2,11 @@ import { getQuerySelector } from '@redux-requests/core';
 import { createSelector } from 'reselect';
 import { convertBalanceFromHex } from '../helpers/balance';
 import { USERS } from '../store/actionTypes';
+import { getCurrentPrice } from './price';
+import { getMassagedTokenData } from './tokens';
 
 export const getPublicKey = ({ user }) => {
-	return user.publicKey;
+	return user.publicKey || '02021172744b5e6bdc83a591b75765712e068e5d40a3be8ae360274fb26503b4ad38';
 };
 
 export const userDetailsSelector = getQuerySelector({ type: USERS.FETCH_USER_DETAILS });
@@ -21,3 +23,22 @@ export const getMassagedUserDetails = createSelector(userDetailsSelector, (userD
 		},
 	};
 });
+
+export const getAccountTotalBalanceInFiat = createSelector(
+	getMassagedUserDetails,
+	getCurrentPrice,
+	getMassagedTokenData,
+	(accountDetails, CSPRPrice, tokensData) => {
+		const CSPRBalance = (accountDetails && accountDetails.balance && accountDetails.balance.displayBalance) || 0;
+		const CSPRValue = CSPRPrice * CSPRBalance;
+		//TODO: should get price for each token, currently no token issue on Casper blockchain and no source as well
+		const tokenPrice = 0;
+		const tokensValue =
+			tokensData && tokensData.length
+				? tokensData.reduce((out, datum) => {
+						return out + ((datum && datum.balance && datum.balance.displayValue) || 0) * tokenPrice;
+				  }, 0)
+				: 0;
+		return CSPRValue + tokensValue;
+	},
+);
--- client/src/web-extension/Common/Account/index.jsx
+++ client/src/web-extension/Common/Account/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getPublicKey, getAccountTotalBalanceInFiat } from '../../../selectors/user';
+import { MiddleTruncatedText } from '../../../components/Common/MiddleTruncatedText';
+import { toFormattedCurrency } from '../../../helpers/format';
 import './index.scss';
 
 export const AccountInfo = () => {
+	//Selectors
+	const publicKey = useSelector(getPublicKey);
+	const totalFiatBalance = useSelector(getAccountTotalBalanceInFiat);
+
 	return (
 		<div className="cd_we_account">
 			<div className="cd_we_account_name">Account 1</div>
-			<div className="cd_we_account_address">01123...123123</div>
-			<div className="cd_we_account_balance">$100</div>
+			<div className="cd_we_account_address">
+				<MiddleTruncatedText end={4}>{publicKey}</MiddleTruncatedText>
+			</div>
+			<div className="cd_we_account_balance">{toFormattedCurrency(totalFiatBalance)}</div>
 		</div>
 	);
 };
