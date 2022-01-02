889bd21cc683e8dd630fa64ec9f289c181ea782e
Thinh Nguyen <thinhnp@ymese.com>
2022-01-02 15:47:54 +0700

    add ledger keys to reducer

--- client/src/store/reducers/ledgerReducer.js
+++ client/src/store/reducers/ledgerReducer.js
@@ -3,12 +3,17 @@ import { LEDGER } from '../actionTypes';
 export default function ledgerReducer(
 	state = {
 		casperApp: null,
+		ledgerKeys: [],
 	},
 	action,
 ) {
 	switch (action.type) {
 		case LEDGER.SET_LEDGER_OPTIONS:
-			return { ...state, casperApp: action.payload.casperApp };
+			return {
+				...state,
+				casperApp: action.payload.casperApp,
+				ledgerKeys: action.payload.ledgerKeys,
+			};
 		default:
 			return state;
 	}
