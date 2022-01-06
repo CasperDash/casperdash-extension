e2c1a292f06c43ae6985c530449be37737f5051d
kiennguyen <kiennguyen@niometrics.com>
2022-01-06 13:50:11 +0700

    remove unused import

--- client/src/components/web-extension/Send/ConfirmSend.jsx
+++ client/src/components/web-extension/Send/ConfirmSend.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
