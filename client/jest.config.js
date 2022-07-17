cae2fe750c9857d25dbe23994bc8f7dcf5213793
Duc Q <torspeedv8@gmail.com>
2022-07-17 23:32:52 +0700

    UPDATE: - Fix up eslint config with jest

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  roots: ["<rootDir>"],
+  "roots": ["<rootDir>"],
   "transform": {
-    "^.+\\.(js|jsx)$": "babel-jest"
+    "^.+\\.[jt]sx?$": "babel-jest"
   },
   "collectCoverageFrom": [
     "src/**/*.{js,jsx,ts,tsx}",
@@ -11,23 +11,12 @@ module.exports = {
   "moduleNameMapper": {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/src/__mocks__/styleMock.js",
-    "^.+\\.(css|scss)$": "<rootDir>/src/__mocks__/styleMock.js",
-    "^assets(.*)$": "<rootDir>/src/assets$1",
-    "^actions(.*)$": "<rootDir>/src/actions$1",
-    "^helpers(.*)$": "<rootDir>/src/helpers$1",
-    "^selectors(.*)$": "<rootDir>/src/selectors$1",
-    "^services(.*)$": "<rootDir>/src/services$1",
-    "^shared(.*)$": "<rootDir>/src/shared$1",
-    "^components(.*)$": "<rootDir>/src/components$1",
-    "^Common(.*)$": "<rootDir>/src/components/Common$1",
-    "^hooks(.*)$": "<rootDir>/src/components/hooks$1",
-    "^web/(.*)$": "<rootDir>/src/components/web$1",
-    "web-extension(.*)$": "<rootDir>/src/components/web-extension$1"
+    "^.+\\.(css|scss)$": "<rootDir>/src/__mocks__/styleMock.js"
   },
   "transformIgnorePatterns": [
     "<rootDir>/node_modules/(?!lodash-es)"
   ],
-  "setupFilesAfterEnv": ["jest-environment-jsdom-global", "jest-extended","<rootDir>/scripts/custom-test-env.js"],
+  "setupFilesAfterEnv": ["jest-extended","<rootDir>/scripts/custom-test-env.js"],
   "watchPlugins": [
     "jest-watch-typeahead/filename",
     "jest-watch-typeahead/testname"
--- client/webpack/commonConfig.js
+++ client/webpack/commonConfig.js
@@ -14,10 +14,7 @@ module.exports = (dir) => ({
 				test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"]
-          }
+          loader: 'babel-loader'
         }
 			},
 			{
