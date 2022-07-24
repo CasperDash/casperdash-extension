e6ca50dd5aad45f7880a4051352d10fae58f37e2
Duc Q <torspeedv8@gmail.com>
2022-07-24 23:10:16 +0700

    UPDATE: - Add cross-env

--- client/src/actions/createWalletActions.utils.js
+++ client/src/actions/createWalletActions.utils.js
@@ -1,6 +1,7 @@
 import reduce from 'lodash-es/reduce';
 import dropRight from 'lodash-es/dropRight';
 import filter from 'lodash-es/filter';
+import { CONSTANTS } from '@cd/shared/constants';
 
 /** Randomize array */
 function shuffle(array) {
@@ -26,9 +27,12 @@ const generateCWHeader = (currentStep, answerSheet) => {
 			const defaultName = 'Double check';
 			if (answerSheet) {
 				/**
-				 * Comment out below line to cheat seeing answer sheet
+				 * Only logging out answer sheet while in development env for debugging purpose
 				 */
-				console.log(`🚀 ~ generateCWHeader ~ answerSheet`, answerSheet);
+				if (CONSTANTS.DEBUG_ENV) {
+					// eslint-disable-next-line
+					console.log(`🚀 ~ generateCWHeader ~ answerSheet`, answerSheet);
+				}
 				const count = filter(answerSheet, (value) => value !== null);
 
 				if (!count?.length) {
@@ -46,17 +50,17 @@ const generateCWHeader = (currentStep, answerSheet) => {
 	}
 };
 
-const generateKeyphraseMap = keyphrase => {
-  const map = new Map();
-  keyphrase.split(" ").forEach((word, index) => map.set(index, word));
-  return map;
+const generateKeyphraseMap = (keyphrase) => {
+	const map = new Map();
+	keyphrase.split(' ').forEach((word, index) => map.set(index, word));
+	return map;
 };
 
 /**
-   * E.g: [1,2,3...11,12]
-   */
-const generateKeyphraseArray = totalKeywords => {
-  return  [...(new Array(totalKeywords)).keys()]
+ * E.g: [1,2,3...11,12]
+ */
+const generateKeyphraseArray = (totalKeywords) => {
+	return [...new Array(totalKeywords).keys()];
 };
 
 const convertKeyphraseToAnswerObject = (keyphrase) => {
@@ -74,38 +78,38 @@ const convertKeyphraseToAnswerObject = (keyphrase) => {
 };
 
 /**
-   * Randomize words from generated keyphrase
-   * Given how many words we'd like to validate,
-   * This will create a random index object using key index only (based on keyphrase generated)
-   * For word conversion, this will be done in actual UI
-   */
-  const onGenerateWordcheck = totalWordCheck => {
-    const initWordKeys = generateKeyphraseArray(totalWordCheck);
-    const randomWordIds = (shuffle(initWordKeys)).splice(0, totalWordCheck);
+ * Randomize words from generated keyphrase
+ * Given how many words we'd like to validate,
+ * This will create a random index object using key index only (based on keyphrase generated)
+ * For word conversion, this will be done in actual UI
+ */
+const onGenerateWordcheck = (totalWordCheck) => {
+	const initWordKeys = generateKeyphraseArray(totalWordCheck);
+	const randomWordIds = shuffle(initWordKeys).splice(0, totalWordCheck);
 
-    /**
-     * Idea:
-     * For each key index:
-     *  - Create another shuffled array from total keyphrase (excluding the current key)
-     *  - Take 2 first elements from the shuffled in Step 1
-     *  - Create a shuffled array from [2 first elements, current key]
-     */
-    let final = {};
-    randomWordIds.forEach(id => {
-      const newWordArr = generateKeyphraseArray(totalWordCheck);
-      const excludedWordIds = newWordArr.filter(k => k !== id);
-      const newRandom = shuffle(excludedWordIds);
-      const remaining = dropRight(newRandom, newRandom.length - 2);
-      final[id] = { answer: id, options: [...shuffle([...remaining, id])]};
-    });
+	/**
+	 * Idea:
+	 * For each key index:
+	 *  - Create another shuffled array from total keyphrase (excluding the current key)
+	 *  - Take 2 first elements from the shuffled in Step 1
+	 *  - Create a shuffled array from [2 first elements, current key]
+	 */
+	let final = {};
+	randomWordIds.forEach((id) => {
+		const newWordArr = generateKeyphraseArray(totalWordCheck);
+		const excludedWordIds = newWordArr.filter((k) => k !== id);
+		const newRandom = shuffle(excludedWordIds);
+		const remaining = dropRight(newRandom, newRandom.length - 2);
+		final[id] = { answer: id, options: [...shuffle([...remaining, id])] };
+	});
 
-    return { checklist: randomWordIds, data: final };
-  }
+	return { checklist: randomWordIds, data: final };
+};
 
 export {
-  onGenerateWordcheck,
-  generateCWHeader,
-  generateKeyphraseArray,
-  generateKeyphraseMap,
-  convertKeyphraseToAnswerObject
-}
+	onGenerateWordcheck,
+	generateCWHeader,
+	generateKeyphraseArray,
+	generateKeyphraseMap,
+	convertKeyphraseToAnswerObject,
+};
