72f4cf36714cb88853bb560a776c666a5a609be0
Duc Q <torspeedv8@gmail.com>
2022-07-25 12:42:06 +0700

    UPDATE: - Fix up typo

--- client/src/shared/formMessages.js
+++ client/src/shared/formMessages.js
@@ -1,6 +1,6 @@
 export default {
 	passwordRequired: 'Password required!',
 	passwordNotMatching: "Passwords don't match",
-	passwordNotStrong: 'Provided password is not strong enought. Please try another',
+	passwordNotStrong: 'Provided password is not strong enough. Please try another',
 	passwordWrong: 'Wrong password provided. Please try again',
 };
