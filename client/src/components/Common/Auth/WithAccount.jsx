174df2dc613d8d5956ab000826899050793906b0
Duc Q <torspeedv8@gmail.com>
2022-08-02 16:05:24 +0700

    UPDATE: - Fix up issue when not found nay local storage for first-time user

--- client/src/components/Common/Auth/WithAccount.jsx
+++ client/src/components/Common/Auth/WithAccount.jsx
@@ -5,32 +5,37 @@ import { getConnectedAccountLocalStorage } from '@cd/actions/userActions';
 import { getPublicKey } from '@cd/selectors/user';
 
 const WithAccount = ({ children }) => {
-  const cacheConnectedAccount = getConnectedAccountLocalStorage();
-  const { publicKey: publicKeyCache, loginOptions: loginOptionsCache } = cacheConnectedAccount;
+	const cacheConnectedAccount = getConnectedAccountLocalStorage();
 
 	// Hook
-  /**
-   * publicKey is cleared after closing extension (Clicking on CD extension icon)
-   * We should store this somewhere else
-   */
+	/**
+	 * publicKey is cleared after closing extension (Clicking on CD extension icon)
+	 * We should store this somewhere else
+	 */
 	const publicKey = useSelector(getPublicKey);
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-    /**
-     * Navigate to `/welcomeBack` screen when found cached User info
-     * Otherwise, redirect back to connect Account screen
-     */
-    if (!publicKey) {
-      if (loginOptionsCache?.userInfo && loginOptionsCache?.userHashingOptions) {
-        navigate('/welcomeBack');
-        return;
-      }
+		if (!cacheConnectedAccount) {
+			navigate('/connectAccount');
+			return;
+		}
 
-      navigate('/connectAccount');
-    }
-	}, [cacheConnectedAccount, publicKey, navigate, dispatch, publicKeyCache, loginOptionsCache]);
+		const { loginOptions: loginOptionsCache } = cacheConnectedAccount;
+		/**
+		 * Navigate to `/welcomeBack` screen when found cached User info
+		 * Otherwise, redirect back to connect Account screen
+		 */
+		if (!publicKey) {
+			if (loginOptionsCache?.userInfo && loginOptionsCache?.userHashingOptions) {
+				navigate('/welcomeBack');
+				return;
+			}
+
+			navigate('/connectAccount');
+		}
+	}, [cacheConnectedAccount, publicKey, navigate, dispatch]);
 
 	return children;
 };
