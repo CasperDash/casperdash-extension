2498a5f37da7173edffa7eaaa2e4b9110abacbff
Duc Q <torspeedv8@gmail.com>
2022-08-11 00:16:02 +0700

    UPDATE: - Create generic OverlayLoader

---
+++ client/src/components/web-extension/Common/OverlayLoader/OverlayLoader.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { TailSpin } from '@cd/common/Spinner';
+
+const OverlayLoader = () => {
+	return (
+		<div
+			style={{
+				background: 'white',
+				height: '100%',
+				position: 'fixed',
+				width: '100%',
+				zIndex: 100,
+				alignItems: 'center',
+				display: 'flex',
+				justifyContent: 'center'
+			}}
+		>
+			<TailSpin />
+		</div>
+	);
+};
+
+export default OverlayLoader;
