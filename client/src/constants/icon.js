50ef7e81dc15c19d6dd3577432462ca9b503d6a7
Duc Q <torspeedv8@gmail.com>
2022-08-17 22:26:05 +0700

    UPDATE: - Fix wrong image paths

--- client/src/constants/icon.js
+++ client/src/constants/icon.js
@@ -1,3 +1,3 @@
-export const SEND_ICON_SMALL = '@cd/assets/images/send-icon-small.svg';
-export const RECEIVE_ICON_SMALL = '@cd/assets/images/receive-icon-small.svg';
-export const STAKING_ICON = '@cd/assets/images/staking-icon.svg';
+export const SEND_ICON_SMALL = 'assets/images/send-icon-small.svg';
+export const RECEIVE_ICON_SMALL = 'assets/images/receive-icon-small.svg';
+export const STAKING_ICON = 'assets/images/staking-icon.svg';
