5b8a3267551fbeed348ec087fc01e5be654f15d7
Duc Q <torspeedv8@gmail.com>
2022-08-25 20:49:57 +0700

    UPDATE: - Move top away syntax into IIFE function

--- client/src/components/hooks/useServiceWorker.js
+++ client/src/components/hooks/useServiceWorker.js
@@ -1,9 +1,11 @@
 let browser;
-if (window?.chrome?.runtime && chrome.runtime.id) {
-  // Code running in a Chrome extension (content script, background page, etc.)
-  console.log(">>> chrome.", chrome.runtime.id);
-  browser = await import("webextension-polyfill");
-}
+(async () => {
+	if (window?.chrome?.runtime && chrome.runtime.id) {
+		// Code running in a Chrome extension (content script, background page, etc.)
+		console.log(">>> chrome.", chrome.runtime.id);
+		browser = await import("webextension-polyfill");
+	}
+})();
 
 const resultHandler = (response = {}) => {
 	if (response.error) {
