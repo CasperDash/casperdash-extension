f118c1423df2cc21bd49c30c81f0c87ad61b7a98
Duc Q <torspeedv8@gmail.com>
2022-09-04 10:18:47 +0700

    UPDATE: - Move heading logic into ImportWallet component

--- client/src/components/web-extension/ImportWallet/ImportWallet.jsx
+++ client/src/components/web-extension/ImportWallet/ImportWallet.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { selectCreateWalletCurrentStep } from "@cd/selectors/createWallet";
+import { useOutletContext } from 'react-router-dom';
+import { selectCreateWalletCurrentStep } from '@cd/selectors/createWallet';
 import CreatePasswordPage from '@cd/web-extension/Common/CreatePasswordPage';
-import ImportWalletPage from "./ImportWalletPage";
-
+import ImportWalletPage from './ImportWalletPage';
 import './ImportWallet.scss';
 
 const ImportWallet = () => {
-  const currentStep = useSelector(selectCreateWalletCurrentStep);
-  return (
-    <section className="cd_we_page--root">
-      {currentStep === 0 && <ImportWalletPage />}
-      {currentStep === 1 && <CreatePasswordPage />}
-    </section>
+	const [, setHeader] = useOutletContext();
+	const currentStep = useSelector(selectCreateWalletCurrentStep);
+	useEffect(() => {
+		if (currentStep === 0) {
+			setHeader('Import keyphrase');
+		}
+
+		if (currentStep === 1) {
+			setHeader('Enter password');
+		}
+	}, [currentStep, setHeader]);
+	return (
+		<section className="cd_we_page--root">
+			{currentStep === 0 && <ImportWalletPage />}
+			{currentStep === 1 && <CreatePasswordPage />}
+		</section>
 	);
 };
 
