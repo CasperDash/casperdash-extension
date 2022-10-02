95129e9cc1f3172b31f71038bc24b7161a848cb3
hoailinh1210 <hoailinh1210@gmail.com>
2022-10-02 11:06:18 +0700

    nit

---
+++ client/src/components/hooks/useLoginPassword.js
@@ -0,0 +1,38 @@
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { onBindingAuthInfo } from '@cd/actions/userActions';
+import { validateReturningUserSW } from '@cd/hooks/useServiceWorker';
+
+const useLoginPassword = () => {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	const onAuthCredentialSuccess = useCallback(
+		(result) => {
+			const { publicKey, userDetails } = result;
+			dispatch(onBindingAuthInfo({ publicKey, user: userDetails }));
+		},
+		[dispatch, navigate],
+	);
+
+	const validateUserCredential = useCallback(async (password) => {
+		if (!password) {
+			return undefined;
+		}
+
+		try {
+			const result = await validateReturningUserSW(password);
+
+			// Similar to useCreateUser
+			return result;
+		} catch (err) {
+			console.error(`🚀 ~ >> ~ err`, err);
+			return undefined;
+		}
+	}, []);
+
+	return { validateUserCredential, onAuthCredentialSuccess };
+};
+
+export default useLoginPassword;
---
+++ client/src/components/web-extension/Common/LoginModal/index.jsx
@@ -0,0 +1,104 @@
+import React, { useState, useCallback } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { Formik } from 'formik';
+import { Button, Form, FormControl } from 'react-bootstrap';
+import messages from '@cd/shared/formMessages';
+import useLoginPassword from '@cd/components/hooks/useLoginPassword';
+import './LoginModal.scss';
+
+const onValidatePassword = (values) => {
+	const errors = {};
+
+	if (!values.password) {
+		errors.password = messages.passwordRequired;
+	}
+
+	return errors;
+};
+
+export const LoginModal = ({ isOpen, onOk }) => {
+	const { onAuthCredentialSuccess, validateUserCredential } = useLoginPassword();
+	const [serverErrors, setServerErrors] = useState(undefined);
+	const onValidate = useCallback((values) => onValidatePassword(values), []);
+	const handleFormSubmit = useCallback(
+		async (values) => {
+			if (values.password) {
+				const result = await validateUserCredential(values.password);
+
+				if (!result) {
+					setServerErrors({ message: messages.passwordWrong });
+					return;
+				}
+
+                console.log('result: ', result);
+
+				result.publicKey && onAuthCredentialSuccess(result);
+                onOk();
+			}
+		},
+		[onAuthCredentialSuccess, validateUserCredential],
+	);
+
+	const onChangeHandler = useCallback(
+		(e, handler) => {
+			if (e?.target?.value && serverErrors) {
+				setServerErrors(undefined);
+			}
+			handler('password', e?.target?.value);
+		},
+		[serverErrors],
+	);
+
+	return (
+		<Modal show={isOpen} className="cd_we_login-modal">
+			<Modal.Header closeButton={false}>
+				<Modal.Title></Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<div className="cd_we_create-wallet-layout--root">
+					<Formik
+						initialValues={{
+							password: '',
+						}}
+						validate={onValidate}
+						onSubmit={handleFormSubmit}
+					>
+						{({ errors, touched, setFieldValue, handleBlur, handleSubmit }) => {
+							return (
+								<div className="cd_we_create-wallet-layout--body">
+									<Form noValidate onSubmit={handleSubmit}>
+										<Form.Group className="mb-3">
+											<Form.Label>Enter password</Form.Label>
+											<FormControl
+												onBlur={handleBlur}
+												onChange={(e) => onChangeHandler(e, setFieldValue)}
+												name="password"
+												type="password"
+												placeholder="Enter password"
+											/>
+											{errors.password && touched.password && (
+												<Form.Text className="invalid-feedback" id="passwordHelpBlock">
+													{errors.password}
+												</Form.Text>
+											)}
+										</Form.Group>
+										{serverErrors && (
+											<Form.Text className="invalid-feedback" id="passwordHelpBlock">
+												{serverErrors.message}
+											</Form.Text>
+										)}
+                                        <div className="cd_we_page--bottom">
+										<Button type="submit" className="cd_we_btn-next" disabled={false}>
+											Unlock
+										</Button>
+									</div>
+									</Form>
+								</div>
+							);
+						}}
+					</Formik>
+				</div>
+			</Modal.Body>
+		</Modal>
+	);
+};
