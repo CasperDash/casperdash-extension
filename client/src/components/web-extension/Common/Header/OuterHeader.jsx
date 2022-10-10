396467ca59f1eb884d8b37649551a090bd68c209
hoailinh1210 <hoailinh1210@gmail.com>
2022-10-10 23:41:45 +0700

    fix: #154 revert currentStep selector & update logic check pathname

--- client/src/components/web-extension/Common/Header/OuterHeader.jsx
+++ client/src/components/web-extension/Common/Header/OuterHeader.jsx
@@ -1,23 +1,25 @@
 import React, { useCallback, useMemo } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { resetWalletCreation } from '@cd/actions/createWalletActions';
 import BackArrow from '@cd/assets/image/back-arrow.svg';
 import './OuterHeader.scss';
+import { selectCreateWalletCurrentStep } from '@cd/selectors/createWallet';
 
 export const OuterHeader = ({ setHeader, header }) => {
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
+	const currentStep = useSelector(selectCreateWalletCurrentStep);
 	const { pathname, state } = useLocation();
-	const shouldShowBackArrow = pathname !== '/welcomeBack' && (pathname === '/createWallet' && currentStep !== 0);
+	const shouldShowBackArrow = pathname !== '/welcomeBack' && (pathname !== '/createWallet' || pathname === '/createWallet' && currentStep !== 0);
 
 	const finalLayoutName = useMemo(() => {
-		if (!state?.name) {
-			return undefined;
-		}
-
 		if (header && header !== '') {
 			return header;
+		}
+
+		if (!state?.name) {
+			return undefined;
 		}
 
 		return state?.name;
