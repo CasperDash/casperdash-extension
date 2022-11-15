3af42dbd1b32857f2f16f23bd9518499624b0f8f
KienNguyen <kiennguyen@Kiens-MacBook-Pro.local>
2022-11-15 23:59:07 +0700

    fix default lock time

--- client/src/store/reducers/settings.test.js
+++ client/src/store/reducers/settings.test.js
@@ -1,3 +1,4 @@
+import { DEFAULT_AUTO_LOCK_TIME } from '@cd/constants/key';
 import settingsReducer from './settings';
 
 describe('SettingReducers', () => {
@@ -5,27 +6,29 @@ describe('SettingReducers', () => {
 		test('Should return new SWITCH_THEME  state', () => {
 			expect(settingsReducer(undefined, { type: 'SETTINGS.SWITCH_THEME', payload: { theme: 'dark' } })).toEqual({
 				theme: 'dark',
-				autoLockTime: 5
+				autoLockTime: DEFAULT_AUTO_LOCK_TIME,
 			});
 		});
-		
+
 		test('Should return current state', () => {
 			expect(settingsReducer({ theme: '' }, { type: 'SETTINGS.TEST', payload: 'dark' })).toEqual({ theme: '' });
-		});		
+		});
 	});
 
 	describe('When set auto lock time', () => {
 		test('Should return lock time with 10 minutes', () => {
-			expect(settingsReducer(undefined, { type: 'SETTINGS.SET_AUTO_LOCK_TIME', payload: { autoLockTime: 10 } })).toEqual({
+			expect(
+				settingsReducer(undefined, { type: 'SETTINGS.SET_AUTO_LOCK_TIME', payload: { autoLockTime: 10 } }),
+			).toEqual({
 				theme: '',
-				autoLockTime: 10
+				autoLockTime: 10,
 			});
 		});
 
 		test('Should return default lock time with 5 minutes', () => {
-			expect(settingsReducer(undefined, { type: 'SETTINGS.TEST', payload: { } })).toEqual({
+			expect(settingsReducer(undefined, { type: 'SETTINGS.TEST', payload: {} })).toEqual({
 				theme: '',
-				autoLockTime: 5
+				autoLockTime: DEFAULT_AUTO_LOCK_TIME,
 			});
 		});
 	});
