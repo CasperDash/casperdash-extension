7b9b65989faea62bdcc8cbbcc9721920837813af
hoailinh1210 <hoailinh1210@gmail.com>
2023-05-30 18:34:58 +0700

    refactor: remove download button and add copy button on recoveryphrase

--- client/src/components/web-extension/CreateWallet/RecoveryPhrasePage.jsx
+++ client/src/components/web-extension/CreateWallet/RecoveryPhrasePage.jsx
@@ -7,9 +7,9 @@ import { generateKeyphrase, setNextStep } from '@cd/actions/createWalletActions'
 import SelectEncryptionType from '@cd/web-extension/Common/SelectEncryptionType';
 import NumberRecoveryWordsSelect from '@cd/web-extension/Common/NumberRecoveryWordsSelect';
 import { selectCreateWalletKeyphraseAsMap, selectCreateWalletKeyphrase } from '@cd/selectors/createWallet';
-import { DownloadButton } from '@cd/components/web-extension/Common/DownloadButton';
 import { NUMBER_OF_RECOVERY_WORDS } from '@cd/constants/key';
 import './RecoveryPhrasePage.scss';
+import CopyButton from '../Common/CopyButton/index';
 
 const RecoveryPhrasePage = () => {
 	const dispatch = useDispatch();
@@ -57,11 +57,9 @@ const RecoveryPhrasePage = () => {
 				</ul>
 			</div>
 			<div className="cd_we_create-keyphrase--actions">
-				<DownloadButton
+				<CopyButton
 					className="cd_we_create-keyphrase__btn"
-					variant="normal"
 					text={keyPhrase}
-					fileName="key_phrase.txt"
 				/>
 				<Button onClick={onClickNextHandler} className="cd_we_create-keyphrase__btn">
 					Next
--- client/src/components/web-extension/RecoveryPhrase/index.jsx
+++ client/src/components/web-extension/RecoveryPhrase/index.jsx
@@ -1,15 +1,17 @@
 import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { DownloadButton } from '@cd/components/web-extension/Common/DownloadButton';
+import { useNavigate } from 'react-router-dom';
+import clsx from 'clsx';
 import EnterPasswordModal from '@cd/web-extension/Common/LoginModal/EnterPasswordModal';
 import { getKeyphrase } from '@cd/components/hooks/useServiceWorker';
-import { useNavigate } from 'react-router-dom';
+import CopyButton from '@cd/web-extension/Common/CopyButton';
 
 import './RecoveryPhrase.scss';
 
 const RecoveryPhrase = () => {
 	const navigate = useNavigate();
 
+	const [isBlurred, setIsBlurred] = useState(true);
 	const [showEnterPassword, setShowEnterPassword] = useState(true);
 	const [keyphrase, setKeyphrase] = useState('');
 
@@ -26,38 +28,64 @@ const RecoveryPhrase = () => {
 	const words = keyphrase ? keyphrase.split(' ') : [];
 
 	return (
-		<div className="cd_we_recovery-keyphrase">
-			<div className="cd_we_recovery-keyphrase__wrapper">
-				<ul className="cd_we_recovery-keyphrase__column">
-					{words?.map((word, index) => (
-						<li className="cd_we_recovery-keyphrase__word" key={`left-${index}`}>
-							<span className="counter">{index + 1}</span>
-							<span className="value">{word}</span>
-						</li>
-					))}
-				</ul>
-				<div className="cd_we_recovery-keyphrase__footer">
-					<DownloadButton
-						className="cd_we_recovery-keyphrase__copy-btn"
-						text={keyphrase}
-						fileName="key_phrase.txt"
-					/>
+			<div className="cd_we_recovery-keyphrase">
+				<div className="cd_we_recovery-keyphrase__wrapper">
+					<div className={clsx({
+						'cd_we_recovery-keyphrase__blur': isBlurred,
+					})}
+			onClick={() => setIsBlurred(false)}
+					>
+						<ul className={
+							clsx('cd_we_recovery-keyphrase__column', {
+								'cd_we_recovery-keyphrase__column--blurred': isBlurred,
+							})
+						}
+						>
+							{words?.map((word, index) => (
+								<li className="cd_we_recovery-keyphrase__word" key={`left-${index}`}>
+									<span className="counter">{index + 1}</span>
+									<span className="value">{word}</span>
+								</li>
+							))}
+						</ul>
+						{
+							isBlurred && (
+								<div className="cd_we_recovery-keyphrase__blur-overlay">
+									Click to reveal secret recovery phrase
+								</div>
+							)
+						}
+					</div>
+					<div className="cd_we_recovery-keyphrase__footer">
+						{
+							!isBlurred && (
+								<CopyButton
+		className="cd_we_recovery-keyphrase__copy-btn"
+		text={keyphrase}
+								/>
+							)
+						}
+					</div>
 				</div>
+				<div className="cd_we_recovery-keyphrase__warning">
+					<div className="cd_we_recovery-keyphrase__warning-text">
+						Please copy your secret recovery phrase and keep it in a safe place. If you lose your device or uninstall the extension, you will need this phrase to recover your wallet.
+					</div>
+				</div>
+				{showEnterPassword && (
+					<EnterPasswordModal
+  isOpen={showEnterPassword}
+  onCloseModal={() => {
+							setShowEnterPassword(false);
+							navigate('/');
+						}}
+  title="View Recovery Phrase"
+  description="Enter password to continue"
+  onSubmitPassword={onViewRecoveryPhrase}
+					/>
+				)}
 			</div>
-
-			{showEnterPassword && (
-				<EnterPasswordModal
-					isOpen={showEnterPassword}
-					onCloseModal={() => {
-						setShowEnterPassword(false);
-						navigate('/');
-					}}
-					title="View Recovery Phrase"
-					description="Enter password to continue"
-					onSubmitPassword={onViewRecoveryPhrase}
-				/>
-			)}
-		</div>
+	
 	);
 };
 
