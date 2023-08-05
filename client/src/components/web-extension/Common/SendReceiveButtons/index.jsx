9522c7129cb5bdaf330b7a42748371e779b73636
kiennguyen <>
2023-08-05 23:23:24 +0700

    add ramp

--- client/src/components/web-extension/Common/SendReceiveButtons/index.jsx
+++ client/src/components/web-extension/Common/SendReceiveButtons/index.jsx
@@ -3,9 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import SendIcon from '@cd/assets/image/send-icon.svg';
 import ReceiveIcon from '@cd/assets/image/receive-icon.svg';
 import './index.scss';
+import { useSelector } from 'react-redux';
+import { getPublicKeyAndLoginOptions } from '@cd/selectors/user';
 
 export const SendReceive = ({ token }) => {
 	const navigate = useNavigate();
+	const { publicKey } = useSelector(getPublicKeyAndLoginOptions);
 
 	const onReceiveClick = () => {
 		navigate('/receive', { state: { name: 'Receive' } });
@@ -13,6 +16,12 @@ export const SendReceive = ({ token }) => {
 
 	const onSendClick = () => {
 		navigate('/send', { state: { name: 'Send', token } });
+	};
+
+	const onBuyClick = () => {
+		window.open(
+			`https://app.demo.ramp.network?hostApiKey=btryzq9sfpr444frq7h3wr3shpj3ov653go3vrgm&hostAppName=CasperDash&hostLogoUrl=https://github.com/CasperDash/casperdash-materials/blob/3b92dd04768a96946e054548c12d9281e56b17d4/media-kit/rw-lg.png?raw=true&userAddress=${publicKey}`,
+		);
 	};
 
 	return (
@@ -28,6 +37,12 @@ export const SendReceive = ({ token }) => {
 					<ReceiveIcon />
 				</div>
 				<div className="cd_we_send_receiver_text">Receive</div>
+			</div>
+			<div className="cd_we_send_receive_item" onClick={onBuyClick}>
+				<div className="cd_we_send_receiver_icon">
+					<ReceiveIcon />
+				</div>
+				<div className="cd_we_send_receiver_text">Buy</div>
 			</div>
 		</div>
 	);
